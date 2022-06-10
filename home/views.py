2bf32a2381dc19e3053743d09e00d7bbf3882b1c
Dibyasom <dibya.secret@gmail.com>
2022-06-10 16:40:00 +0530

    Event ordering improved.

--- home/views.py
+++ home/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
+# Importing models
+from core.models.podcast import Podcast
+
+# Extract Video-ID from the video URL
+video_id_extractor = lambda x: str(x)[str(x).rindex("=") + 1 : ]
+
 # Create your views here.
 def landing_page(request):
-    '''
+    """
     Render the landing page.
-    '''
-    return render(request, "landing_page.html")
+    """
+    # Get all podcasts
+    podcasts = Podcast.objects.all()
+    video_ids = [video_id_extractor(podcast.stream_link) for podcast in podcasts]
+    podcasts_w_vid = zip(podcasts, video_ids)
+
+    # Render view
+    return render(request, "landing_page.html", {"podcasts": podcasts_w_vid})
